d8285e550c717cb070443e79adfb2e84d21e3589
WesleyFirmino <wesleydasilva_firmino@live.com>
2020-08-25 14:11:23 -0300

    Insert style TeacherForm/ Insert components: Input, Selec and Textarea

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,10 +1,60 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Textearea from '../../components/Textarea';
+import Select from '../../components/Select';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+import './styles.css';
+
 
 function TeacherForm() {
     return  (
         <div id="page-teacher-form" className="container">
-            <PageHeader title="Que incrível que você quer dar aulas."/>
+            <PageHeader
+             title="Que incrível que você quer dar aulas."
+             description="O primeiro passo é preencher esse formulário de inscrição"
+             />
+
+             <main>
+                 <fieldset>
+                     <legend>Seus dados</legend>
+
+                     <Input name="name" label="Nome completo"/>
+                     <Input name="avatar" label="Avatar"/>
+                     <Input name="whatsapp" label="WhatsApp"/>
+                     <Textearea name="bio" label="Biográfia"/>
+                 </fieldset>
+
+                 <fieldset>
+                     <legend>Sobre a aula</legend>
+
+                     <Select 
+                        name="subject" 
+                        label="Matéria"
+                        options={[
+                            {value: 'Artes', label: 'Artes'},
+                            {value: 'Biologia', label: 'Biologia'},
+                            {value: 'Ciência', label: 'Ciência'},
+                            {value: 'Física', label: 'Física'},
+                            {value: 'Matemática', label: 'Matemática'},
+                            {value: 'Química', label: 'Química'}
+                        ]}
+                     />
+                     <Input name="cost" label="Custo da sua hora por aula"/>
+                 </fieldset>
+
+                 <footer>
+                     <p>
+                         <img src={warningIcon} alt="Aviso importante"/>
+                         Importante! <br/>
+                         Preencha todos os dados
+                     </p>
+                     <button type="button">
+                         Salvar cadastro
+                     </button>
+                 </footer>
+             </main>
         </div>
     )
 }
